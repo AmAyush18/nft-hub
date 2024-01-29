9615b05f6d1533cc866d11cae72b257e4b77b322
Ayush Dwivedi <amayush0818@gmail.com>
2024-01-29 18:45:19 +0530

    Responsiveness of Creators

--- src/components/Home/Creators.jsx
+++ src/components/Home/Creators.jsx
@@ -10,8 +10,8 @@ function Creators() {
             <div className="w-full flex flex-wrap justify-between mt-6">
                 {
                     creators.map((creator) => (
-                        <div key={creator.id} className="w-[24%] flex flex-col gap-y-3 justify-center items-center h-[240px] bg-[#3b3b3b] mb-4 rounded-2xl">
-                            <img src={creator.avatar} alt="..." className='w-[130px] h-[130px] rounded-full object-cover' />
+                        <div key={creator.id} className="w-full md:w-[48%] lg:w-[24%] flex flex-col gap-y-3 justify-center items-center h-[252px] md:h-[240px] bg-[#3b3b3b] mb-4 rounded-2xl">
+                            <img src={creator.avatar} alt="..." className='w-[150px] h-[150px] md:w-[130px] md:h-[130px] rounded-full object-cover' />
                             <p className='text-[16px] font-[500]'>{creator.title}</p>
                             <p className='text-xs font-[400] text-[#ccc]'>Total Sales: <span className='text-white'>{creator.sales + " ETH"} </span></p>
                         </div>

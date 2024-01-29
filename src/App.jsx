29df0c135e70bce163708142e418637127dcef2a
Ayush Dwivedi <amayush0818@gmail.com>
2024-01-29 13:15:53 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='w-full min-h-screen bg-[#2b2b2b] text-white'>
+      NFT-Hub
+    </div>
+  )
+}
+
+export default App
